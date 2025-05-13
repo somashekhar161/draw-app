47a578465e77a7851e9fa8b99cd203cd87f07e33
Somashekar B R <sanjayrao161@gmail.com>
2025-05-14 01:42:33 +0530

    added basic ws and http

---
+++ apps/ws-backend/src/config.ts
@@ -0,0 +1,1 @@
+export const JWT_SECRET = "1@241233@%^@145asda12312asdads2ed";
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,11 +1,28 @@
 import { WebSocketServer } from "ws";
-
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", function connection(ws) {
+wss.on("connection", function connection(ws, request) {
+  const url = request.url;
+  if (!url) {
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token");
+  if (!token) {
+    return;
+  }
+
+  const decoded = jwt.verify(token, JWT_SECRET);
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
+
   
-    ws.on("message", function message(data) {
+  ws.on("message", function message(data) {
     ws.send("pong");
   });
-
 });
